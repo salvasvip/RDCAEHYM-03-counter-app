ffd173df18c0016f820f4826f541a42ab66f0547
Salvador Vilca <slvdrvlc@gmail.com>
2021-07-14 23:24:18 -0400

    Initial commit

---
+++ src/CounterApp.js
@@ -0,0 +1,28 @@
+// rscp REACT STATELESS COMPONENT PROPTYPES (SNIPPET)
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value = 10}) => {
+
+    const [counter, setCounter] = useState(10);
+    // handleAdd
+    const handleAdd = () => setCounter(counter+1)
+    const handleReset = () => setCounter(value)
+    const handleSubtract = () => setCounter(counter-1)
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{counter}</h2>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubtract}>-1</button>
+        </>
+    );
+};
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+};
+
+export default CounterApp;
